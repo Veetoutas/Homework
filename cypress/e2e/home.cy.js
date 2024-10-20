bcf5a387b734789247070b66fb4448ec7e4f090a
Veetoutas <vyt.matulevicius@gmail.com>
2024-10-21 02:16:50 +0300

    commit Homework

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,27 @@
+const customerInfo = {
+  country: 'Lithuania',
+  street: 'Studentu',
+  email: 'test@test.com',
+  name: 'Tester',
+  phone: '+111111111',
+  customField: 'I am really hungry today :)',
+  selfServiceParcelAddress: 'Kauno RIMI Studentų paštomatas, Studentų g. 19'
+};
+
+describe('Muffin shop', () => {
+  beforeEach(() => {
+    cy.visit("https://lightgrey-antelope-m7vwozwl8xf7l3y2.builder-preview.com");
+  });
+
+  it('should complete the full shopping and checkout process', () => {
+    cy.navigateToShopFromHome();
+    cy.sortByPriceHighToLow();
+    cy.addFirstItemToBasket();
+    cy.proceedToCheckout();
+    cy.fillShippingDetails(customerInfo);
+    cy.fillContactInfo(customerInfo);
+    cy.clickButtonByData("checkout-contactinformation-continue");
+    cy.clickButtonByData("checkout-paymentmethods-placeorder");
+    cy.getByData("ecommerce-modal-checkout-success-order").contains('Thank you for your order');
+  });
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,53 @@
+Cypress.Commands.add("getByData", (selector) => {
+    return cy.get(`[data-qa="${selector}"]`);
+  });
+
+Cypress.Commands.add("getById", (selector) => {
+    return cy.get(`[id="${selector}"]`);
+});
+
+Cypress.Commands.add("getByClass", (selector) => {
+    return cy.get(`[class="${selector}"]`);
+});
+
+Cypress.Commands.add("getByInput", (selector) => {
+    return cy.get(`input[${selector}"]`);
+});
+
+Cypress.Commands.add('navigateToShopFromHome', () => {
+    cy.getByData("navigation-item-shop").contains("Shop").click();
+});
+
+Cypress.Commands.add('sortByPriceHighToLow', () => {
+    cy.getById("sorting-options").select("Price (high to low)");
+});
+
+Cypress.Commands.add('addFirstItemToBasket', () => {
+    cy.getByData("product-list-section-item-image").first().click();
+    cy.clickButtonByData("productsection-btn-addtobag");
+});
+
+Cypress.Commands.add('proceedToCheckout', () => {
+    cy.clickButtonByData("shoppingcart-btn-checkout");
+});
+
+Cypress.Commands.add('fillShippingDetails', (customerInfo) => {
+    cy.getByInput('id="destination').type(customerInfo.country);
+    cy.getByClass("v-list-item-title").contains(customerInfo.country).click();
+    cy.getByInput('placeholder="Choose address').type(customerInfo.street);
+    cy.getByClass("v-list-item-title").contains('Studentų g. 19').click();
+    cy.getByData('checkout-shippingoptions-parcelselect').contains(customerInfo.selfServiceParcelAddress);
+    cy.clickButtonByData("checkout-shippingdetails-continue");
+});
+
+Cypress.Commands.add('fillContactInfo', (customerInfo) => {
+    cy.getByInput('id="email').type(customerInfo.email);
+    cy.getByInput('id="name').type(customerInfo.name);
+    cy.getByInput('id="phone').type(customerInfo.phone);
+    cy.getByInput('id="customFieldValue').type(customerInfo.customField);    
+});
+
+Cypress.Commands.add('clickButtonByData', (dataAttribute) => {
+    cy.getByData(dataAttribute).click();
+});
+  
